630dcc6d23078573e522e74f40183f0ad260d349
Jeferson Vieira <jeferson.vm1981@gmail.com>
2024-03-22 15:30:29 -0300

    Finalizando os exercícios

--- front-react-integracao-backend-exercicios/base-ex2/src/pages/SignIn/index.js
+++ front-react-integracao-backend-exercicios/base-ex2/src/pages/SignIn/index.js
@@ -1,32 +1,53 @@
-import { useState } from 'react';
-import './styles.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
+import api from "../../services/api";
+import { setItem, getItem } from "../../utils/storage";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [Passsword, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+    try {
+      if (!email || !password) {
+        console.log("Preencha todos os campos");
+        return;
+      }
+      const response = await api.post("/login", { email, password });
+      const { token } = response.data;
+      setItem("token", token);
+      navigate("/main");
+    } catch (error) {
+      console.error(error);
+    }
   }
 
+  useEffect(() => {
+    const token = getItem("token");
+    if (token) {
+      navigate("/main");
+    }
+  }, [navigate]);
+
   return (
-    <div className='container'>
+    <div className="container">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder='Email'
+          placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
-          placeholder='Passsword'
-          value={Passsword}
+          placeholder="Passsword"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button>
-          Login
-        </button>
+        <button>Login</button>
       </form>
     </div>
   );
--- front-react-integracao-backend-exercicios/base-ex2/src/routes.js
+++ front-react-integracao-backend-exercicios/base-ex2/src/routes.js
@@ -1,19 +1,20 @@
-import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
-import Main from './pages/Main';
-import SignIn from './pages/SignIn';
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import Main from "./pages/Main";
+import SignIn from "./pages/SignIn";
+import { getItem } from "./utils/storage";
 
 function ProtectedRoutes({ redirectTo }) {
-  const isAuthenticated = true;
+  const isAuthenticated = getItem("token");
 
-  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />;
 }
 
 function MainRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<SignIn />} />
+      <Route path="/" element={<SignIn />} />
       <Route element={<ProtectedRoutes redirectTo="/" />}>
-        <Route path='/main' element={<Main />} />
+        <Route path="/main" element={<Main />} />
       </Route>
     </Routes>
   );
